b59d8fb41204a6e5d9b17495dec8aa259beb4528
rafael.oliveira <rafael.oliveira@huia.haus>
2020-10-28 17:06:35 -0300

    Adicionando loaders, plugins e module.

--- README.md
+++ README.md
@@ -44,5 +44,51 @@ module.exports = {
 }
 ``` 
 
-# Configurar ambiente de desenvolvimento
-2. Executar comando <code>npm i webpack-dev-server --save-dev</code>
+# Configurar ambientes
+### Desenvolvimento
+1. Executar comando <code>npm i webpack-dev-server --save-dev</code>
+2. Aletere o arquivo `package.json` chave "scripts" insira o seguinte conteúdo.
+```
+{
+    "dev": "webpack-dev-server --open --mode development",
+    "staging": "echo \"Success: mode staging\" && exit 0",
+    "prod": "echo \"Success: mode production\" && exit 0",
+    "test": "echo \"Success: mode test\" && exit 0"
+}
+```
+2. Execute ```npm run dev``` para iniciar o servidor local de desenvolvimento.
+
+# Entry
+
+```
+entry: './app/src/index.js',
+```
+
+# Output
+```
+output: {
+  filename: 'main.js',
+  path: path.resolve(__dirname, 'dist'),
+},
+```
+
+
+# Devtools
+
+# Optimization
+
+# Module
+Módulos de cofiguração para identificar e carregar arquivos dos mais diversos tipos.
+Cada módulo possui um teste para identificação da extensão de arquivo assim com as 
+regras de carregamento e tratamento.
+
+# Plugins
+Plugins são extonsões do webpack que permitem a adição de mais funcionalidades
+a ferramenta.
+
+### HtmlWebpackPlugin
+Cria um arquivo HTML5 para incorporar os bundles criados pelo webpack.
+
+### MiniCssExtractPlugin
+Utilizado para extrair arquivos de estilos de dentro do JS e separá-los em arquivos
+css separados.
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,75 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './app/src/index.js',
+  devtool: "teste",
+  optimization: {
+    minimize: true,
+    splitChunks: { chunks: "all" },
+  },
   output: {
-    filename: 'main.js',
+    filename: 'main.[hash].js',
     path: path.resolve(__dirname, 'dist'),
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.(png|gif|jpg|mp4)$/,
+        loader: "url-loader",
+        options: {
+          limit: 8000,
+        },
+      },
+      {
+        test: /\.(ttf|eot|woff|woff2)$/,
+        use: {
+          loader: "file-loader",
+        },
+      },
+      {
+        test: /\.(ogv|mp4|m4v|webm)$/,
+        use: {
+          loader: "file-loader",
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          { loader: "css-loader" },
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              sassOptions: {
+                outputStyle: "compressed",
+              },
+            },
+          },
+        ],
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "main.[hash].css",
+    })
+  ]
 };
